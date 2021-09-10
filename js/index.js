47396ca7a1f4a476259d65f268c8a19a18320f3f
nikl4auss <nicolasmarinelli@protonmail.com>
2021-09-10 20:31:14 -0300

    Finished the frontend challenge

---
+++ js/index.js
@@ -0,0 +1,40 @@
+const emailContainer = document.querySelector(
+  ".subscribe-form__mail-container"
+);
+const emailInput = document.querySelector(".subscribe-form__input");
+const sendButton = document.querySelector(".subscribe-form__button");
+
+console.log();
+
+sendButton.onclick = () => {
+  isValid(emailInput);
+};
+
+let validateEmail = (email) => {
+  console.log(email.value);
+  let regex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  let test = regex.test(email.value);
+  return test;
+};
+
+let warningText = () => {
+  let warning = document.createElement("span");
+  warning.classList.add("warning-text");
+  warning.textContent = "Please provide a valid email address";
+  return warning;
+};
+
+let isValid = (email) => {
+  if (!validateEmail(email)) {
+    let warning = warningText();
+    emailContainer.append(warning);
+    emailInput.classList.add("subscribe-form__input--warning");
+  } else {
+    let warning = emailContainer.querySelector(".warning-text");
+    if (warning) {
+      emailContainer.removeChild(warning);
+      emailInput.classList.remove("subscribe-form__input--warning");
+    }
+  }
+};
